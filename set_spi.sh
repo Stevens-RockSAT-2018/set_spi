f57e21a04cd62f837eb9423482d48f4feea9770c
ShoopIsAFailure <zach_shoop@yahoo.com>
2018-04-10 19:33:09 -0400

    first commit

---
+++ set_spi.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+config-pin P9.17 spi_cs
+config-pin P9.18 spi
+config-pin P9.21 spi
+config-pin P9.22 spi_sclk
+
+config-pin P8.11 out
+config-pin P8.12 out
+config-pin P8.13 out
+config-pin P8.14 out
+config-pin P8.15 out
+config-pin P8.16 out
+config-pin P8.17 out
+config-pin P8.18 out
